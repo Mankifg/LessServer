4d370a7f5cec6a6094ab8a45b8f89b496e38f477
Mankifg <83959255+Mankifg@users.noreply.github.com>
2025-08-10 17:42:41 +0200

    somewhat working code

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import game_utils
 
 
 import asyncio
-from websockets.asyncio.server import serve
+from websockets.asyncio.server import serve,broadcast
 
 import logging 
 
@@ -37,24 +37,109 @@ player_color = "w"
 global games
 games = {}
 
-async def start_game(websocket,msg):
-    #? create new game
-    """
-    {
-        "type": "create,
-        // "ident":uuid 
+async def generate_update_msg(packaged_game:list) -> dict:
+    board, pos, color_to_move, move_power = packaged_game
+    return {
+        "type":"update",
+        "b10_board":board,
+        "lbp":pos,
+        "to_move":color_to_move,
+        "move_power":move_power,
     }
-    """
+
+
+
+async def execute_move(move,game):
+    return True,1
+
+async def error(websocket,message):
+    event = {
+        "type":"error",
+        "message":message
+    }
+    await websocket.send(json.dumps(event))
+
+async def game_over(packaged_game):
+    return False
+ 
+
+async def replay(connected,packaged_game):
+    print("sending starting data to both")
+    board, pos, color_to_move, move_power = packaged_game
+    event = {
+        "type":"update",
+        "b10_board":board,
+        "lbp":pos,
+        "to_move":color_to_move,
+        "move_power":move_power,
+    }
+    
+    broadcast(connected,json.dumps(event))
+
+    
+
+async def play(websocket,packaged_game,connected):
+    async for message in websocket:
+        event = json.loads(message)
+        
+        if (event["type"] != "play"):
+            print(f"something is wrong, servers wanted to recive play package, but it got {event}")
+        
+        print(">",event)
+        
+        legal,resp = execute_move(packaged_game,event)
+        
+        if not legal:
+            error(websocket,"move not legal")
+
+        event = generate_update_msg(packaged_game)
+        
+        broadcast(connected,json.dumps(event))
+        
+        if (game_over(packaged_game)):
+        
+            event = {
+                "type":"win",
+                "player":1,}
+        
+            broadcast(connected,json.dumps(event))
+            
+        
+        
+
+
+async def start_game(websocket):
+    #? create new game
+
     #! white_key = game_utils.new_uuid()
     #! black_key = game_utils.new_uuid()
 
     white_key = "woite"
     black_key = "bluck"
 
+    connected = {websocket}
+
     b10_board = game_utils.new_b10_board()
     game_id = game_utils.new_game_id()
+    packaged_game = b10_board, START_POS, START_COLOR, START_MOVE_POWER
 
-    games.update(
+    games[game_id] = packaged_game,connected
+    
+    try:
+        event  = {
+            "type":"init",
+            "join":game_id,
+        }
+        
+        await websocket.send(json.dumps(event))
+        
+        await play(websocket,packaged_game,1,connected)
+
+    finally:
+        print(f"game with id: {game_id} ended")
+        del games[game_id]
+    
+    '''games.update(
         {
             game_id: {
                 "game": [b10_board, START_POS, START_COLOR, START_MOVE_POWER],
@@ -62,9 +147,9 @@ async def start_game(websocket,msg):
                 "black_key": black_key,
             }
         }
-    )
+    )'''
 
-    print(games)
+    print(f"{games=}")
 
     """
     for i, v in functions.get_wall_moves(b10_board):
@@ -75,7 +160,7 @@ async def start_game(websocket,msg):
         print(game_utils.from_arry_notation(i), v)
     """
     
-    await websocket.send(json.dumps(
+    '''await websocket.send(json.dumps(
     {
         "error":False,
         "game_id": game_id,
@@ -84,35 +169,35 @@ async def start_game(websocket,msg):
          
         "you": "w",
         "ident": white_key,
-    }))
+    }))'''
     
     
       
 
-async def join_game(websocket,msg):
-
-    game_id = str(game_id)
-    if not game_id in games.keys():
-        return {"s": False, "game_end": False, "code": "game_id is not found"}
-
-    game_obj = games[game_id]["game"]
-    black_key = games[game_id]["black_key"]
-
-    await websocket.send(json.dumps(
-        {
-        "error":False,
-        "game_id": game_id,
-        "game_end": False,  # todo this shuld be fiixed
-        "game": game_obj,
+async def join_game(websocket,join_key):
+    try:
+        packaged_game,connected = games[join_key]
+    except KeyError:
+        await error(websocket,"game not found")
+        print("game with id",join_key,"not found")
+        return
     
-        "your": "b",
-        "ident": black_key,
-    }    ))
+    connected.add(websocket)
+    try:
+        await replay(connected,packaged_game)
+    
+        await play(websocket,packaged_game,2,connected)
+    
+    finally:
+        connected.remove(websocket)
+        
+        
+    
 
 
 async def handler(websocket):
     async for message in websocket:
-        logging.log("Recived:",message)
+        print(f"Recived: >{message}")
         
         try:
             msg_obj = json.loads(message)
@@ -126,12 +211,13 @@ async def handler(websocket):
             return
         
         if msg_obj["type"] == "create":
-            await start_game(websocket,msg_obj)
+            await start_game(websocket)
         
         elif msg_obj["type"] == "join":
-            await join_game(websocket,msg_obj)
+            await join_game(websocket,msg_obj["key"])
             
-            
+        else:
+            print(f"recived {msg_obj['type']}, idk what it is")
 """            
     
 
